3bc26bf6bd9ef6062811e43f76f3292c54c18947
totoro440 <wilsun.messagerie@gmail.com>
2026-04-20 14:21:52 +0400

    first commit

---
+++ mastermind.js
@@ -0,0 +1,28 @@
+export class Mastermind{
+    constructor(nbColor=5,life=9,nbItem=4){
+        //tableau à trouver
+        this.tabToFind = [];
+        //tableau à essayer
+        this.tabToTry = [];
+        //initialisation des tableaux par des 0 pour fixer leur taille
+        for (let i = 0; i < nbItem; i++) {
+            this.tabToFind.push(0);
+            this.tabToTry.push(0);    
+        }
+        //initialisation du nombre d'essaie pour trouver la solution
+        this.life = life;
+        //initialisation du nombre de couleur possible pour un item 
+        this.nbColor = nbColor;
+    }
+
+    //véritable initialisation du jeu
+    init(){
+        for (let i = 0; i < this.tabToFind.length; i++) {
+            this.tabToFind[i]= Math.floor(Math.random()*this.nbColor+1);
+        };
+    }
+}
+
+let test = new Mastermind();
+test.init();
+console.log(test);
