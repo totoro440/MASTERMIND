6edc4cb9feaa1581bb45768546306ab4275d8a64
totoro440 <wilsun.messagerie@gmail.com>
2026-04-23 14:37:40 +0400

    add domclues

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,7 @@
 "use strict";
-let myMastermind = new Mastermind(); // le mastermind
-let currentSection = 1;
-myMastermind.init();
+let myMastermind = new Mastermind(); // création le mastermind
+let currentSection = 1; // le numéro de la section en cours
+myMastermind.init(); // initialisation du mastermin
 
 // création du dom
 let createDom = (mind) => {
@@ -11,9 +11,6 @@ let createDom = (mind) => {
         section.dataset.life = i+1;
         let wp = document.createElement("div");
         wp.classList=['wellplaced'];
-        for (let j = 0; j < mind.getSize(); j++) {
-            wp.appendChild(document.createElement('div'));
-        }
         section.appendChild(wp);
         for (let j = 0; j < mind.getSize(); j++) {
             let data = document.createElement('div');
@@ -22,9 +19,6 @@ let createDom = (mind) => {
         }
         let mp = document.createElement("div");
         mp.classList=['misplaced'];
-        for (let j = 0; j < mind.getSize(); j++) {
-            mp.appendChild(document.createElement('div'));
-        }
         section.appendChild(mp);
         main.appendChild(section);
         section.classList=['hidden'];
@@ -43,6 +37,19 @@ let getTabToTry = (mind,actif) => {
     return tab;
 }
 
+//afficher les indices dans le dom 
+function printDomClues(mind,actif){
+    let sectionActif = document.querySelector(`section[data-life='${actif}']`);
+    console.log(sectionActif);
+    let [w,m] = mind.getClues();
+    console.log(w,m);
+    for (let j = 0; j < w; j++) {
+        sectionActif.querySelector('.wellplaced').appendChild(document.createElement('div'));
+    }
+    for (let j = 0; j < m; j++) {
+        sectionActif.querySelector('.misplaced').appendChild(document.createElement('div'));
+    }
+}
 
 // fonction pour changer la couleur de l'affichage
 let nextColor = (mind, color) => {
@@ -73,7 +80,7 @@ document.querySelector('button').addEventListener("click", ()=>{
     myMastermind.setTabToTry(getTabToTry(myMastermind,currentSection));
     console.log(getTabToTry(myMastermind,currentSection));
     myMastermind.play();
-    console.log(myMastermind.getClues());
+    printDomClues(myMastermind,currentSection);
     for (const element of document.querySelector(`section[data-life='${currentSection}']`).querySelectorAll(`div[data-color]`)) {
         element.removeEventListener("click", domNextColor);   
     }
